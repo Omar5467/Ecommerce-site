8d1c41998f2240aced7c29993a384b384a01d27c
Omar <Omarmoha5467@gmail.com>
2026-05-30 15:08:04 +0300

    Saving cart

---
+++ cart/cart.py
@@ -0,0 +1,20 @@
+class Cart():
+    def __init__(self, request):
+        self.session = request.session
+        cart = request.session.get('cart')
+        if 'cart' not in request.session:
+            cart = self.session['cart'] = {}
+        self.cart = cart
+
+    def __len__(self):
+        return sum(int(item['quantity']) for item in self.cart.values())
+
+    def add(self, product, product_quantity):
+        product_id = product.id
+        if product_id in self.cart:
+            self.cart[product_id]['quantity'] = product_quantity
+        else:
+            self.cart[product_id] = {'price': str(product.price), 'quantity': product_quantity}
+        
+        self.session.modified = True
+            
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .cart import Cart
+from ecomapp.models import Product
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def cart_add(request):
+    cart = Cart(request)
     print("Cart button clicked!")
+    if request.method == 'POST':
+        product_id = request.POST.get('product_id')
+        product_quantity = request.POST.get('product_quantity')
+        print(f"Product added to cart: {product_id}, Quantity: {product_quantity}")
+        product = get_object_or_404(Product, id=product_id)
+        # cart.add(product, product_quantity)
+        cart.add(product=product, product_quantity=product_quantity)
     return JsonResponse({'Message':'Add to cart button is clicked!'})
