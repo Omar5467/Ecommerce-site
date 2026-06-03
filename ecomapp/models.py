443c80afd1a78282eeb43eee29c5f271c99c6a49
Omar <Omarmoha5467@gmail.com>
2026-06-03 10:22:14 +0300

    Delete, update cart and create userapp

---
+++ ecomapp/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
+
+
+
+
+# Create your models here.
+class Product(models.Model):
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.slug])
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    image = models.ImageField(upload_to='product_images/')
+    slug = models.SlugField(unique=True,blank=True)
+    stock = models.IntegerField()
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            slug = base_slug
+            counter = 1
+            while Product.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+
+        super().save(*args, **kwargs)
