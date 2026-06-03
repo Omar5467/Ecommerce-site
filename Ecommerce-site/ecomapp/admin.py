f198b6fbade7b5490f36e3e9ae4b26af2685b46e
Omar5467 <omarmoha5467@gmail.com>
2026-06-03 10:20:46 +0300

    Delete Ecommerce-site directory
    This commit was made incorrectly

--- Ecommerce-site/ecomapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from ecomapp.models import Product
-
-# Register your models here.
-admin.site.register(Product)
--- Ecommerce-site/ecomapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<slug:slug>/', views.product_detail, name='product_detail'),
-]
